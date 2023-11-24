c429ed7f3f7155e62685492260e2bdb92d4aef1b
Alec Roelke <alec.roelke@gmail.com>
2023-11-24 17:16:26 -0500

    Add keyboard controls to skip to region edge

--- src/battle/Cursor.cs
+++ src/battle/Cursor.cs
@@ -34,17 +34,24 @@ public partial class Cursor : Sprite2D
             Cell = Map.CellOf(mm.Position);
         else
         {
-            Vector2I target = (Vector2I)Input.GetVector("cursor_left", "cursor_right", "cursor_up", "cursor_down").Round();
-            if (!@event.IsEcho() && target != _mask)
-                target = new(_mask.X == 0 ? target.X : 0, _mask.Y == 0 ? target.Y : 0);
-            if (target != Vector2.Zero)
-                Cell += target;
+            Vector2I skip = (Vector2I)Input.GetVector("cursor_skip_left", "cursor_skip_right", "cursor_skip_up", "cursor_skip_down").Round();
+            if (skip != Vector2.Zero)
+                Cell = new(
+                    skip.X < 0 ? 0 : skip.X > 0 ? Map.Size.X - 1 : Cell.X,
+                    skip.Y < 0 ? 0 : skip.Y > 0 ? Map.Size.Y - 1 : Cell.Y
+                );
+
+            Vector2I move = (Vector2I)Input.GetVector("cursor_left", "cursor_right", "cursor_up", "cursor_down").Round();
+            if (!@event.IsEcho() && move != _mask)
+                move = new(_mask.X == 0 ? move.X : 0, _mask.Y == 0 ? move.Y : 0);
+            if (move != Vector2.Zero)
+                Cell += move;
             if (!@event.IsEcho())
             {
                 if (@event.IsPressed())
-                    _mask = new(_mask.X | target.Abs().X, _mask.Y | target.Abs().Y);
+                    _mask = new(_mask.X | move.Abs().X, _mask.Y | move.Abs().Y);
                 else if (@event.IsReleased())
-                    _mask = new(_mask.X & target.Abs().X, _mask.Y & target.Abs().Y);
+                    _mask = new(_mask.X & move.Abs().X, _mask.Y & move.Abs().Y);
                 else
                     _mask = Vector2I.Zero;
             }
