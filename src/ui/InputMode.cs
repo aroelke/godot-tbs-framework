c1e84e4e9005299ceba88a2b6c544d41c5062172
aroelke <alec.roelke@gmail.com>
2023-12-02 17:13:08 -0500

    Move controller watching to an autoload node

--- src/ui/InputMode.cs
+++
@@ -1,7 +0,0 @@
-namespace ui;
-
-/// <summary>
-/// Method used for moving the virtual mouse cursor: the real mouse, using digital input (e.g. keyboard keys, controller dpad), or
-/// using analog inputs (e.g. controller analog sticks).
-/// </summary>
-public enum InputMode { Mouse, Digital, Analog }
