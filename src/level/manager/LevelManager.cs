55ba63ff9394f32b767410d721437020c881c622
aroelke <alec.roelke@gmail.com>
2023-12-08 20:15:01 -0500

    Add empty LevelManager and getter interface

---
+++ src/level/manager/LevelManager.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace level.manager;
+
+/// <summary>
+/// Manages the setup of and objects inside a level and provides them information about it.  Is "global" in a way in that
+/// all objects in the level may be able to see it and request information from it, but each level has its own manager.
+/// </summary>
+public partial class LevelManager : Node2D
+{
+}
