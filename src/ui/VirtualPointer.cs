867522b53097d9e95d3834550c8879ce6630cd51
Alec Roelke <alec.roelke@gmail.com>
2023-12-10 18:06:32 -0500

    Add virtual pointer "acceleration" factor

--- src/ui/VirtualPointer.cs
+++ src/ui/VirtualPointer.cs
@@ -12,10 +12,33 @@ public partial class VirtualPointer : TextureRect
     [Signal] public delegate void PointerMovedEventHandler(Vector2 previous, Vector2 current);
 
     private InputManager _inputManager = null;
+    private bool _accelerate = false;
+
     private InputManager InputManager => _inputManager ??= GetNode<InputManager>("/root/InputManager");
 
-    /// <summary>Speed in pixels/second the cursor moves when in analog mode.</summary>
+    /// <summary>Speed in pixels/second the pointer moves when in analog mode.</summary>
     [Export] public double Speed = 600;
+
+    /// <summary>Multiplier applied to the pointer speed when the accelerate button is held down in analog mode.</summary>
+    [Export] public double Acceleration = 3;
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (InputManager.Mode == input.InputMode.Analog)
+        {
+            if (Input.IsActionJustPressed("cursor_analog_accelerate"))
+            {
+                _accelerate = true;
+                GetViewport().SetInputAsHandled();
+            }
+            if (Input.IsActionJustReleased("cursor_analog_accelerate"))
+            {
+                _accelerate = false;
+                GetViewport().SetInputAsHandled();
+            }
+        }
+    }
 
     public override void _Process(double delta)
     {
@@ -24,7 +47,8 @@ public partial class VirtualPointer : TextureRect
         Vector2 old = Position;
         if (InputManager.Mode == input.InputMode.Analog)
         {
-            Position = (Position + (InputManager.GetAnalogVector()*(float)(Speed*delta))).Clamp(GetViewportRect().Position, GetViewportRect().End);
+            double speed = _accelerate ? (Speed*Acceleration) : Speed;
+            Position = (Position + (InputManager.GetAnalogVector()*(float)(speed*delta))).Clamp(GetViewportRect().Position, GetViewportRect().End);
         }
         if (Position != old)
             EmitSignal(SignalName.PointerMoved, old, Position);
