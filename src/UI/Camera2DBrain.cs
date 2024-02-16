5d36209402737a6779a6a07b617dbd77d9351af1
aroelke <alec.roelke@gmail.com>
2024-02-16 10:32:47 -0500

    Add comments to camera controller

--- src/UI/Camera2DBrain.cs
+++ src/UI/Camera2DBrain.cs
@@ -2,6 +2,12 @@ using Godot;
 
 namespace UI;
 
+/// <summary>
+/// "Brain" controlling the camera. Given a target, it will follow it and smoothly move the camera to continue tracking it. It has two zones: a "dead zone,"
+/// which defines the area where the target can be and the camera won't move, and a "soft zone," which defines an area outside the dead zone where the camera
+/// will move smoothly to put the target back into the dead zone. If the target is outside that, the camera will jump to get the target into the soft zone.
+/// The brain can also control zooming.
+/// </summary>
 [Icon("res://icons/Camera2DBrain.svg"), Tool]
 public partial class Camera2DBrain : Node2D
 {
@@ -9,7 +15,7 @@ public partial class Camera2DBrain : Node2D
     /// <param name="deadzone">Rectangle defining the dead zone.</param>
     /// <param name="limits">Limits where the center of the camera can be.</param>
     /// <returns>The position the center of the camera should move to.</returns>
-    private static Vector2 GetMoveTarget(Vector2 distance, Rect2 deadzone, Rect2 limits)
+    private static Vector2 GetMoveTargetPosition(Vector2 distance, Rect2 deadzone, Rect2 limits)
     {
         Rect2 moveBox = new Rect2().Expand((distance - distance.Clamp(deadzone.Position, deadzone.End)).Clamp(limits.Position, limits.End));
         return distance.Clamp(moveBox.Position, moveBox.End);
@@ -21,9 +27,11 @@ public partial class Camera2DBrain : Node2D
     private Vector2 _zoom = Vector2.One;
     private Rect2I _limits = new(-1000000, -1000000, 2000000, 2000000);
 
+    /// <summary>Object the camera is tracking. Can be null to not track anything.</summary>
     [ExportGroup("Camera")]
     [Export] public Node2D Target = null;
 
+    /// <summary>Camera zoom. Ratio of world pixel size to real pixel size (so a zoom of 2 presents everything in double size).</summary>
     [ExportGroup("Camera")]
     [Export] public Vector2 Zoom
     {
@@ -36,6 +44,7 @@ public partial class Camera2DBrain : Node2D
         }
     }
 
+    /// <summary>Box bounding the area in the world that the camera is allowed to see.</summary>
     [ExportGroup("Camera")]
     [Export(PropertyHint.None, "suffix:px")] public Rect2I Limits
     {
@@ -52,14 +61,20 @@ public partial class Camera2DBrain : Node2D
     }
 
     [ExportGroup("Dead Zone", "DeadZone")]
+
+    /// <summary>Left margin of the dead zone, as a fraction of the distance from the center of the screen to the edge.</summary>
     [Export(PropertyHint.Range, "0, 1")] public float DeadZoneLeft = 0.5f;
 
+    /// <summary>Top margin of the dead zone, as a fraction of the distance from the center of the screen to the edge.</summary>
     [Export(PropertyHint.Range, "0, 1")] public float DeadZoneTop = 0.5f;
 
+    /// <summary>Right margin of the dead zone, as a fraction of the distance from the center of the screen to the edge.</summary>
     [Export(PropertyHint.Range, "0, 1")] public float DeadZoneRight = 0.5f;
 
+    /// <summary>Bottom margin of the dead zone, as a fraction of the distance from the center of the screen to the edge.</summary>
     [Export(PropertyHint.Range, "0, 1")] public float DeadZoneBottom = 0.5f;
 
+    /// <summary>Speed the camera should move to get the target to re-enter the dead zone.</summary>
     [Export(PropertyHint.None, "suffix:px/s")] public double DeadZoneSmoothSpeed = 7.5f;
 
     /// <returns>The viewport rectangle projected onto the world.</returns>
@@ -72,6 +87,7 @@ public partial class Camera2DBrain : Node2D
         return (Rect2)Limits with { Position = Limits.Position + projected.Size/2 - Position, End = Limits.End - projected.Size/2 - Position };
     }
 
+    /// <returns>The rectangle defining the dead zone in the world (rather than on screen).</returns>
     public Rect2 GetDeadZone()
     {
         Rect2 viewport = Camera.GetViewportRect();
@@ -110,7 +126,7 @@ public partial class Camera2DBrain : Node2D
             {
                 DrawCircle(targetRelative, 3, Colors.Red);
                 DrawCircle(targetRelative.Clamp(localDeadzone.Position, localDeadzone.End), 3, Colors.Purple);
-                DrawCircle(GetMoveTarget(targetRelative, localDeadzone, bounds), 3, Colors.Blue);
+                DrawCircle(GetMoveTargetPosition(targetRelative, localDeadzone, bounds), 3, Colors.Blue);
             }
         }
     }
@@ -129,7 +145,7 @@ public partial class Camera2DBrain : Node2D
             {
                 Vector2 targetRelative = Target.Position - Position;
                 if (!localDeadzone.HasPoint(targetRelative))
-                    Position = Position.Lerp(Position + GetMoveTarget(targetRelative, localDeadzone, bounds), (float)(DeadZoneSmoothSpeed*delta));
+                    Position = Position.Lerp(Position + GetMoveTargetPosition(targetRelative, localDeadzone, bounds), (float)(DeadZoneSmoothSpeed*delta));
             }
             QueueRedraw();
         }
