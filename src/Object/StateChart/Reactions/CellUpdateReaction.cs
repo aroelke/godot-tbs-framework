91235c1dd9f25c83e048b9d2c8bd90c33f6357a4
Alec Roelke <alec.roelke@gmail.com>
2024-04-20 18:00:16 -0400

    Add comments to CellUpdateReaction

--- src/Object/StateChart/Reactions/CellUpdateReaction.cs
+++ src/Object/StateChart/Reactions/CellUpdateReaction.cs
@@ -2,8 +2,14 @@ using Godot;
 
 namespace  Object.StateChart.Reactions;
 
+/// <summary>
+/// Allows a <see cref="States.State"/> to react to events regarding individual <see cref="Level.Map.Grid"/> cells. The name of the reaction
+/// node should be used to indicate what the update represents.
+/// </summary>
 public partial class CellUpdateReaction : Reaction
 {
+    /// <summary>Signals that an update has been made to a cell.</summary>
+    /// <param name="cell">Cell that was updated.</param>
     [Signal] public delegate void StateCellUpdatedEventHandler(Vector2I cell);
 
     public void OnCellUpdated(Vector2I cell)
