3ffdf04789e927c098962ab99d056bfd91c72f61
Alec Roelke <alec.roelke@gmail.com>
2024-04-21 07:25:20 -0400

    Fix Grid comment references

--- src/Level/Map/Grid.cs
+++ src/Level/Map/Grid.cs
@@ -11,16 +11,16 @@ namespace Level.Map;
 [Tool]
 public partial class Grid : TileMap
 {
-    /// <summary><c>TileSet</c> layer containing ground tiles.</summary>
+    /// <summary><see cref="TileSet"/> layer containing ground tiles.</summary>
     public int GroundLayer { get; private set; } = -1;
 
-    /// <summary><c>TileSet</c> layer containing terrain tiles. This is the layer that will be used to define terrain effects.</summary>
+    /// <summary><see cref="TileSet"/> layer containing terrain tiles. This is the layer that will be used to define terrain effects.</summary>
     public int TerrainLayer { get; private set; } = -1;
 
     /// <summary>Default terrain to use when it isn't placed explicitly on the map.</summary>
     [Export] public Terrain DefaultTerrain = null;
 
-    /// <summary>Grid cell dimensions derived from the tile set.  If there is no tileset, the size is zero.</summary>
+    /// <summary>Grid cell dimensions derived from the <see cref="TileSet"/>.  If there is no <see cref="TileSet"/>, the size is zero.</summary>
     public Vector2 CellSize => TileSet?.TileSize ?? Vector2.Zero;
 
     /// <summary>Grid dimensions. Both elements should be positive.</summary>
@@ -31,35 +31,35 @@ public partial class Grid : TileMap
 
     /// <summary>Check if a cell offset is in the grid.</summary>
     /// <param name="offset">offset to check.</param>
-    /// <returns><c>true</c> if the offset is within the grid bounds, and <c>false</c> otherwise.</returns>
+    /// <returns><c>true</c> if the <paramref name="offset"/> is within the grid bounds, and <c>false</c> otherwise.</returns>
     public bool Contains(Vector2I offset) => new Rect2I(Vector2I.Zero, Size).HasPoint(offset);
 
     /// <summary>Find the cell offset closest to the given one inside the grid.</summary>
     /// <param name="cell">Cell offset to clamp.
-    /// <returns>The cell offset clamped to be inside the grid bounds using <c>Vector2I.Clamp</c></returns>
+    /// <returns>The cell <paramref name="offset"/> clamped to be inside the grid bounds using <c>Vector2I.Clamp</c></returns>
     public Vector2I Clamp(Vector2I offset) => offset.Clamp(Vector2I.Zero, Size - Vector2I.One);
 
     /// <summary>Find the position in pixels of a cell offset.</summary>
     /// <param name="offset">Cell offset to use for calculation (can be outside grid bounds).</param>
-    /// <returns>The position, in pixels of the upper-left corner of the grid cell.</returns>
+    /// <returns>The position, in pixels, of the upper-left corner of the grid cell.</returns>
     public Vector2 PositionOf(Vector2I offset) => offset*CellSize;
 
     /// <summary>Find the cell offset of a pixel position.</summary>
     /// <param name="position">Position in world pixels.</param>
-    /// <returns>The coordinates of the cell containing the pixel point (can be outside grid bounds).</returns>
+    /// <returns>The coordinates of the cell containing the pixel <paramref name="position"/> (can be outside grid bounds).</returns>
     public Vector2I CellOf(Vector2 position) => (Vector2I)(position/CellSize);
 
     /// <summary>Snap a position to a grid cell.</summary>
     /// <param name="position">Position in world pixels.</param>
-    /// <returns>The position of the upper-left corner of the cell containing the given position.</returns>
+    /// <returns>The position of the upper-left corner of the cell containing the given <paramref name="position"/>.</returns>
     public Vector2 Snap(Vector2 position) => PositionOf(CellOf(position));
 
-    /// <returns>The terrain information for a cell, or <c>DefaultTerrain</c> if the terrain hasn't been set.</returns>
-    /// <exception cref="IndexOutOfRangeException">If the cell is outside the grid.</exception>
+    /// <returns>The terrain information for a cell, or <see cref="DefaultTerrain"/> if the terrain hasn't been set.</returns>
+    /// <exception cref="IndexOutOfRangeException">If the <paramref name="cell"/> is outside the grid.</exception>
     public Terrain GetTerrain(Vector2I cell) => GetCellTileData(TerrainLayer, cell)?.GetCustomData("terrain").As<Terrain>() ?? DefaultTerrain;
 
     /// <param name="cell">Coordinates of the cell.</param>
-    /// <returns>A unique ID within this map of the given cell.</returns>
+    /// <returns>A unique ID within this map of the given <paramref name="cell"/>.</returns>
     public int CellId(Vector2I cell) => cell.X*Size.X + cell.Y;
 
     /// <param name="cell">Coordinates of the cell.</param>
@@ -71,26 +71,26 @@ public partial class Grid : TileMap
     /// path.
     /// </summary>
     /// <param name="path">List of cells to sum up.</param>
-    /// <returns>The sum of the cost of each cell in the collection.</returns>
+    /// <returns>The sum of the cost of each cell in the <paramref name="path"/>.</returns>
     public int Cost(IEnumerable<Vector2I> path) => path.Select((c) => GetTerrain(c).Cost).Sum();
 
     /// <summary>Find all the cells that are exactly a specified Manhattan distance away from a center cell.</summary>
     /// <param name="cell">Cell at the center of the range.</param>
-    /// <param name="range">Distance away from the center cell to search.</param>
-    /// <returns>A collection of cells that are on the grid and exactly the specified distance away from the center.</returns>
-    public IEnumerable<Vector2I> GetCellsAtRange(Vector2I cell, int range)
+    /// <param name="distance">Distance away from the center cell to search.</param>
+    /// <returns>A collection of cells that are on the grid and exactly the specified <paramref name="distance"/> away from the center <paramref name="cell"/>.</returns>
+    public IEnumerable<Vector2I> GetCellsAtRange(Vector2I cell, int distance)
     {
         HashSet<Vector2I> cells = new();
-        for (int i = 0; i < range; i++)
+        for (int i = 0; i < distance; i++)
         {
             Vector2I target;
-            if (Contains(target = cell + new Vector2I(-range + i, -i)))
+            if (Contains(target = cell + new Vector2I(-distance + i, -i)))
                 cells.Add(target);
-            if (Contains(target = cell + new Vector2I(i, -range + i)))
+            if (Contains(target = cell + new Vector2I(i, -distance + i)))
                 cells.Add(target);
-            if (Contains(target = cell + new Vector2I(range - i, i)))
+            if (Contains(target = cell + new Vector2I(distance - i, i)))
                 cells.Add(target);
-            if (Contains(target = cell + new Vector2I(-i, range - i)))
+            if (Contains(target = cell + new Vector2I(-i, distance - i)))
                 cells.Add(target);
         }
         return cells;
