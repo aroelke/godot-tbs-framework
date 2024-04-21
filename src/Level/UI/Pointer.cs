c076c6e816113c42777d25044e69542fd39fbb4c
Alec Roelke <alec.roelke@gmail.com>
2024-04-21 10:45:27 -0400

    Fix Pointer comment references

--- src/Level/UI/Pointer.cs
+++ src/Level/UI/Pointer.cs
@@ -10,8 +10,8 @@ namespace Level.UI;
 
 /// <summary>
 /// Virtual mouse pointer that lives in the game world but has a projection onto the UI canvas and is controlled via analog stick.
-/// Is only visible during analog control; during digital control, it and the main mouse become invisible in favor of the cursor,
-/// and during mouse control, the system mouse is visible.
+/// Is only visible during analog control; during digital control, it and the main mouse become invisible in favor of the
+/// <see cref="Object.Cursor"/>; during mouse control the system mouse is visible.
 /// </summary>
 [Tool]
 public partial class Pointer : BoundedNode2D
@@ -21,34 +21,24 @@ public partial class Pointer : BoundedNode2D
     [Signal] public delegate void PointerMovedEventHandler(Vector2 position);
 
     private InputMode _prevMode = default;
-    private Rect2I _bounds = new(0, 0, 0, 0);
     private bool _accelerate = false;
     private TextureRect _mouse = null;
     private bool _tracking = true;
 
-    /// <summary>Convert a position in the level to a position in the viewport.</summary>
-    /// <param name="world">Level position.</param>
-    /// <returns>Position in the viewport that's at the same place as the one in the level.</returns>
+    /// <summary>Convert a position in the <see cref="World"/> to a position in the <see cref="Viewport"/>.</summary>
+    /// <param name="world"><see cref="World"/> position.</param>
+    /// <returns>Position in the <see cref="Viewport"/> that's at the same place as the one in the <see cref="World"/>.</returns>
     private Vector2 WorldToViewport(Vector2 world) => World.GetGlobalTransformWithCanvas()*world;
 
-    /// <summary>Convert a position in the viewport to a position in the level.</summary>
-    /// <param name="viewport">Viewport position.</param>
-    /// <returns>Position in the level that's at the same place as the one in the viewport.</returns>
+    /// <summary>Convert a position in the <see cref="Viewport"/> to a position in the <see cref="World"/>.</summary>
+    /// <param name="viewport"><see cref="Viewport"/> position.</param>
+    /// <returns>Position in the <see cref="World"/> that's at the same place as the one in the <see cref="Viewport"/>.</returns>
     private Vector2 ViewportToWorld(Vector2 viewport) => World.GetGlobalTransformWithCanvas().AffineInverse()*viewport;
 
-    /// <summary>Bounding rectangle where the cursor is allowed to move.</summary>
-    [Export] public Rect2I Bounds
-    {
-        get => _bounds;
-        set
-        {
-            if (_bounds != value)
-            {
-                _bounds = value;
-            }
-        }
-    }
+    /// <summary>Bounding rectangle where the pointer is allowed to move.</summary>
+    [Export] public Rect2I Bounds = new(0, 0, 0, 0);
 
+    /// <summary>Scene containing the pointer and things it can point to.</summary>
     [Export] public CanvasItem World = null;
 
     /// <summary>Speed in screen pixels/second the pointer moves when in analog mode.</summary>
@@ -79,7 +69,7 @@ public partial class Pointer : BoundedNode2D
     [ExportGroup("Input Actions/Movement")]
     [Export] public InputActionReference AccelerateAction = new();
 
-    /// <summary>Position of the pointer relative to the viewport.</summary>
+    /// <summary>Position of the pointer relative to the <see cref="Viewport"/>.</summary>
     public Vector2 ViewportPosition
     {
         get => Engine.IsEditorHint() ? GlobalPosition : WorldToViewport(GlobalPosition);
@@ -98,10 +88,11 @@ public partial class Pointer : BoundedNode2D
         }
     }
 
-    /// <summary>The pointer is just a point, but it has to have a zero area so the camera can focus on it.</summary>
+    /// <inheritdoc cref="BoundedNode2D.Size"/>
+    /// <remarks>The pointer is just a point, but it has to have a zero area so the camera can focus on it.</remarks>
     public override Vector2 Size { get => Vector2.Zero; set {}}
 
-    /// <summary>Move the cursor to a new location that's not bounded by <c>Bounds</c>, and update listeners that the move occurred.</summary>
+    /// <summary>Move the cursor to a new location that's not bounded by <see cref="Bounds"/>, and update listeners that the move occurred.</summary>
     /// <param name="position">Position to warp to.</param>
     public void Warp(Vector2 position)
     {
@@ -133,12 +124,12 @@ public partial class Pointer : BoundedNode2D
         _prevMode = mode;
     }
 
-    /// <summary>When the mouse enters the screen, warp to its entry position.</summary>
-    /// <param name="position">Position the mouse entered the screen on.</param>
+    /// <summary>When the mouse enters the <see cref="Viewport"/>, warp to its entry position.</summary>
+    /// <param name="position">Position the mouse entered the <see cref="Viewport"/> on.</param>
     public void OnMouseEntered(Vector2 position) => Warp(ViewportToWorld(position));
 
-    /// <summary>When the mouse exits the screen, warp to edge of the screen near where it exited.</summary>
-    /// <param name="position">Position on screen close to where the mouse exited.</param>
+    /// <summary>When the mouse exits the <see cref="Viewport"/> , warp to edge of the <see cref="Viewport"/> near where it exited.</summary>
+    /// <param name="position">Position on <see cref="Viewport"/> close to where the mouse exited.</param>
     public void OnMouseExited(Vector2 position) => Warp(ViewportToWorld(position));
 
     /// <summary>When the cursor moves during digital input, warp to its location.</summary>
