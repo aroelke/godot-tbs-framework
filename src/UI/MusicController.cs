09dcbadb0230c53ce1a5c7f52af485d5c5cf43bb
Alec Roelke <alec.roelke@gmail.com>
2024-04-25 11:46:36 -0400

    Add comments to MusicController

--- src/UI/MusicController.cs
+++ src/UI/MusicController.cs
@@ -2,12 +2,19 @@ using Godot;
 
 namespace UI;
 
+/// <summary>Global controller for background music.</summary>
 public partial class MusicController : AudioStreamPlayer
 {
     private static MusicController _singleton = null;
 
+    /// <summary>
+    /// Reference to the auto-loaded music controller to help with signal connection. Other functions and properties should be accessed via
+    /// static methods.
+    /// </summary>
     public static MusicController Singleton => _singleton ??= ((SceneTree)Engine.GetMainLoop()).Root.GetNode<MusicController>("MusicController");
 
+    /// <summary>Play a song. If the song is already playing, it won't restart.</summary>
+    /// <param name="music">Song to play. If <c>null</c>, the current song won't stop.</param>
     public static void Play(AudioStream music=null)
     {
         if (music is not null)
@@ -22,5 +29,6 @@ public partial class MusicController : AudioStreamPlayer
             ((AudioStreamPlayer)_singleton).Play();
     }
 
+    /// <summary>Stop playing the current song.</summary>
     public static new void Stop() => ((AudioStreamPlayer)Singleton).Stop();
 }
