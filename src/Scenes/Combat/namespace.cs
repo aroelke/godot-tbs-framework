4c594e684c0f2b176f70ee920b5f5174c734c317
aroelke <alec.roelke@gmail.com>
2024-05-03 22:49:15 -0400

    Create Combat namespace

---
+++ src/Scenes/Combat/namespace.cs
@@ -0,0 +1,8 @@
+namespace Scenes
+{
+    namespace Combat
+    {
+        /// <summary>Objects used to display and choreograph combat.</summary>
+        internal static class NamespaceDoc {}
+    }
+}
