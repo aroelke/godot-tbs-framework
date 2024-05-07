33f6d916c91acfeab49ecea242e5048fcb3d896a
aroelke <alec.roelke@gmail.com>
2024-05-07 18:03:57 -0400

    Add rudimentary initial combat attack animation

---
+++ src/Scenes/Combat/Animations/namespace.cs
@@ -0,0 +1,11 @@
+namespace Scenes
+{
+    namespace Combat
+    {
+        namespace Animations
+        {
+            /// <summary>Animations to display during combat, usually tied to character, class, or magic.</summary>
+            internal static class NamespaceDoc {}
+        }
+    }
+}
