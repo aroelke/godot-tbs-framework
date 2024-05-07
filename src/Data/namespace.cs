8e2fe4db6e06ffd7e051a5bb56eb19c768d03768
aroelke <alec.roelke@gmail.com>
2024-05-07 18:48:20 -0400

    Create a namespace for scene-independent data

---
+++ src/Data/namespace.cs
@@ -0,0 +1,5 @@
+namespace Data
+{
+    /// <summary>Data structures that are used in multiple different types of scenes, often passed between them.</summary>
+    internal static class NamespaceDoc {}
+}
