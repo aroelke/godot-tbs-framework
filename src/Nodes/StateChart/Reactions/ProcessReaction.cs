99968a00d25c8a8c3d5632557130206cb86a7e16
aroelke <alec.roelke@gmail.com>
2024-06-25 16:01:58 -0400

    Comment ProcessReaction

--- src/Nodes/StateChart/Reactions/ProcessReaction.cs
+++ src/Nodes/StateChart/Reactions/ProcessReaction.cs
@@ -1,9 +1,13 @@
 using Godot;
+using Nodes.StateChart.States;
 
 namespace Nodes.StateChart.Reactions;
 
+/// <summary>Allows a <see cref="State"/> to perform a task every process cycle.</summary>
 public partial class ProcessReaction : Reaction
 {
+    /// <summary>Signals that a process cycle has executed while the <see cref="State"/> is active.</summary>
+    /// <param name="delta">Time since the last process step (whether or not the <see cref="State"/> was active).</param>
     [Signal] public delegate void StateProcessEventHandler(double delta);
 
     public override void _Process(double delta)
