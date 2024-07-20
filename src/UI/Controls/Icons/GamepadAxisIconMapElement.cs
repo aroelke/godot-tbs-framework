616a528f8944ac1e69abfd415fba1ba0d7a79224
aroelke <alec.roelke@gmail.com>
2024-07-20 18:06:50 -0400

    Add comments to GamepadAxisIconMapElement

--- src/UI/Controls/Icons/GamepadAxisIconMapElement.cs
+++ src/UI/Controls/Icons/GamepadAxisIconMapElement.cs
@@ -2,10 +2,13 @@ using Godot;
 
 namespace UI.Controls.Icons;
 
+/// <summary>Individual mapping of a game pad (by name) onto its button icon map.</summary>
 [GlobalClass, Tool]
 public partial class GamepadAxisIconMapElement : Resource
 {
+    /// <summary>Name of the game pad to map icons to.</summary>
     [Export] public string GamepadName = "";
 
+    /// <summary>Mapping of input actions onto game pad button icons for the game pad.</summary>
     [Export] public IndividualGamepadAxisIconMap IconMap = null;
 }
