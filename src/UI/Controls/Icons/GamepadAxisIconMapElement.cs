ae52a9224d85571732008c7c2d861a2be86a8dba
aroelke <alec.roelke@gmail.com>
2024-07-20 18:18:18 -0400

    Fix game pad axis map comments

--- src/UI/Controls/Icons/GamepadAxisIconMapElement.cs
+++ src/UI/Controls/Icons/GamepadAxisIconMapElement.cs
@@ -2,13 +2,13 @@ using Godot;
 
 namespace UI.Controls.Icons;
 
-/// <summary>Individual mapping of a game pad (by name) onto its button icon map.</summary>
+/// <summary>Individual mapping of a game pad (by name) onto its axis icon map.</summary>
 [GlobalClass, Tool]
 public partial class GamepadAxisIconMapElement : Resource
 {
     /// <summary>Name of the game pad to map icons to.</summary>
     [Export] public string GamepadName = "";
 
-    /// <summary>Mapping of input actions onto game pad button icons for the game pad.</summary>
+    /// <summary>Mapping of input actions onto game pad axis icons for the game pad.</summary>
     [Export] public IndividualGamepadAxisIconMap IconMap = null;
 }
