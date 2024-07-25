729a834de8283dabced87f9789253a5c87005f49
aroelke <alec.roelke@gmail.com>
2024-07-25 14:02:44 -0400

    Use InputActions const in FFComponent rather than ref

--- src/Nodes/Components/FastForwardComponent.cs
+++ src/Nodes/Components/FastForwardComponent.cs
@@ -12,16 +12,13 @@ public partial class FastForwardComponent : Node
     /// <summary>Signals that fast-forwarding has ended.</summary>
     [Signal] public delegate void DecelerateEventHandler();
 
-    /// <summary>Input action to toggle fast-forwarding.</summary>
-    [Export] public InputActionReference Action = null;
-
     /// <summary>Whether or not fast-forwarding is active.</summary>
     public bool Active { get; private set; } = false;
 
     public override void _EnterTree()
     {
         base._EnterTree();
-        if (Input.IsActionPressed(Action))
+        if (Input.IsActionPressed(InputActions.FastForward))
         {
             Active = true;
             EmitSignal(SignalName.Accelerate);
@@ -31,12 +28,12 @@ public partial class FastForwardComponent : Node
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (@event.IsActionPressed(Action))
+        if (@event.IsActionPressed(InputActions.FastForward))
         {
             Active = true;
             EmitSignal(SignalName.Accelerate);
         }
-        else if (@event.IsActionReleased(Action))
+        else if (@event.IsActionReleased(InputActions.FastForward))
         {
             Active = false;
             EmitSignal(SignalName.Decelerate);
