355cf9ca5e23703b02a02b3b03d3c46d1b454594
aroelke <alec.roelke@gmail.com>
2024-07-30 11:36:49 -0400

    Create TbsTemplate namespace for everything

---
+++ src/TbsTemplate/namespace.cs
@@ -0,0 +1,5 @@
+namespace TbsTemplate
+{
+    /// <summary>Defines mostly-generic nodes and interfaces for creating a grid-based, turn-based strategy game.</summary>
+    internal static class NamespaceDoc {}
+}
