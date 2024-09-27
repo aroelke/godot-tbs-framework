6ec91b8a00332cb9604d6d789fb51d5eae0ac1af
aroelke <alec.roelke@gmail.com>
2024-09-27 16:35:39 -0400

    Change default condition property values to null

--- src/TbsTemplate/Nodes/StateChart/Reactions/Reaction.cs
+++ src/TbsTemplate/Nodes/StateChart/Reactions/Reaction.cs
@@ -12,7 +12,7 @@ public partial class Reaction : ChartNode
     private State _state = null;
 
     /// <summary>Condition guarding the reaction; must be satisfied for the reaction to occur when the trigger arrives.</summary>
-    [Export] public Condition Condition = new UnCondition();
+    [Export] public Condition Condition = null;
 
     /// <summary>Whether or not the reaction can trigger.</summary>
     public bool Active => (_state?.Active ?? false) && Condition.IsSatisfied(this);
