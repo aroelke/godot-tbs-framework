5ab77bad9f630966d077d6d6bae76ae713868ac3
aroelke <alec.roelke@gmail.com>
2024-11-07 13:59:57 -0500

    Add "survive X turns" objective

---
+++ src/TbsTemplate/Scenes/Level/Objectives/TimeObjective.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace TbsTemplate.Scenes.Level.Objectives;
+
+/// <summary>Objective that is completed after a certain number of turns from the beginning of the level have elapsed.</summary>
+[Tool]
+public partial class TimeObjective : Objective
+{
+    /// <summary>Number of turns to elapse before completion.</summary>
+    [Export] public int Turns = 0;
+
+    public override string Description => $"Survive {Turns} Turns";
+
+    public void OnTurnBegan(int turn) => Completed = turn > Turns;
+
+    public override string[] _GetConfigurationWarnings()
+    {
+        List<string> warnings = new(base._GetConfigurationWarnings() ?? []);
+
+        if (Turns < 0)
+            warnings.Add("Turn count should not be negative.");
+        else if (Turns == 0)
+            warnings.Add("Time objective will always be completed.");
+
+        return [.. warnings];
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Completed = Turns <= 0;
+    }
+}
