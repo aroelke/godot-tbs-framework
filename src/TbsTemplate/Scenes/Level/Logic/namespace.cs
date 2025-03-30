44b58312e5f032a308af337d6a0ce5c624d41e91
aroelke <alec.roelke@gmail.com>
2025-03-30 14:15:59 -0400

    Create namespace for level logic

---
+++ src/TbsTemplate/Scenes/Level/Logic/namespace.cs
@@ -0,0 +1,14 @@
+namespace TbsTemplate
+{
+    namespace Scenes
+    {
+        namespace Level
+        {
+            namespace Logic
+            {
+                /// <summary>Defines logic governing levels that can be applied to <see cref="Godot.Node"/>s or other classes to execute or simulate events.</summary>
+                static internal class NamespaceDoc {}
+            }
+        }
+    }
+}
