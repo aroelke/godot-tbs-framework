8c514f6e0ac44686502c9f7336480479524537a4
aroelke <alec.roelke@gmail.com>
2025-04-01 11:51:40 -0400

    Create namespace for logic components

---
+++ src/TbsTemplate/Scenes/Level/Logic/Components/namespace.cs
@@ -0,0 +1,17 @@
+namespace TbsTemplate
+{
+    namespace Scenes
+    {
+        namespace Level
+        {
+            namespace Logic
+            {
+                namespace Components
+                {
+                    /// <summary>Components for game logic classes to define common behavior.</summary>
+                    static internal class NamespaceDoc {}
+                }
+            }
+        }
+    }
+}
