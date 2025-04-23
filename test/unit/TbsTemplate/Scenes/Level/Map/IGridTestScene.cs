95606b6acd93c35e1e91c806a35217933e8d0f5d
aroelke <alec.roelke@gmail.com>
2025-04-23 11:25:55 -0400

    Test IGrid.Contains

--- test/unit/TbsTemplate/Scenes/Level/Map/IGridTestScene.cs
+++ test/unit/TbsTemplate/Scenes/Level/Map/IGridTestScene.cs
@@ -11,10 +11,10 @@ namespace TbsTemplate.Scenes.Level.Map.Test;
 [Test]
 public partial class IGridTestScene : Node
 {
-    private readonly record struct TestGrid(Vector2I Size, IDictionary<Vector2I, Terrain> Terrain) : IGrid
+    private readonly record struct TestGrid(Vector2I Size, Dictionary<Vector2I, Terrain> Terrain) : IGrid
     {
         // Functions to test
-        public bool Contains(Vector2I cell) => IGrid.Contains(this, Size);
+        public bool Contains(Vector2I cell) => IGrid.Contains(this, cell);
         public IEnumerable<Vector2I> GetCellsAtDistance(Vector2I cell, int distance) => IGrid.GetCellsAtDistance(this, cell, distance);
         public int PathCost(IEnumerable<Vector2I> path) => IGrid.PathCost(this, path);
 
@@ -25,4 +25,10 @@ public partial class IGridTestScene : Node
         public IImmutableDictionary<Vector2I, IUnit> GetOccupantUnits() => throw new NotImplementedException();
         public bool IsTraversable(Vector2I cell, Faction faction) => throw new NotImplementedException();
     }
+
+    // "Contains" tests
+    [Test] public void TestGridContainsZero() => Assert.IsTrue(new TestGrid(Vector2I.One, []).Contains(Vector2I.Zero));
+    [Test] public void TestGridContainsEnd() => Assert.IsTrue(new TestGrid(new(3, 5), []).Contains(new(2, 4)));
+    [Test] public void TestGridDoesntContainMinusOne() => Assert.IsFalse(new TestGrid(Vector2I.One, []).Contains(-Vector2I.One));
+    [Test] public void TestGridDoesntContainSize() => Assert.IsFalse(new TestGrid(new(3, 5), []).Contains(new(3, 5)));
 }
