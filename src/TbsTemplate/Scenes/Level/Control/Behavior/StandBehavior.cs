7c50fc82f791d1902d9081b7ce1c48f174a7e8aa
aroelke <alec.roelke@gmail.com>
2025-04-24 16:57:25 -0400

    Add basic support action to StandBehavior

--- src/TbsTemplate/Scenes/Level/Control/Behavior/StandBehavior.cs
+++ src/TbsTemplate/Scenes/Level/Control/Behavior/StandBehavior.cs
@@ -13,22 +13,30 @@ public partial class StandBehavior : UnitBehavior
     /// <summary>Whether or not the unit should attack enemies in range.</summary>
     [Export] public bool AttackInRange = false;
 
+    /// <summary>Whether or not the unit should support allies in range.</summary>
+    [Export] public bool SupportInRange = true;
+
     public override IEnumerable<Vector2I> Destinations(IUnit unit, IGrid grid) => [unit.Cell];
 
     public override Dictionary<StringName, IEnumerable<Vector2I>> Actions(IUnit unit, IGrid grid)
     {
+        Dictionary<StringName, IEnumerable<Vector2I>> actions = [];
+
         if (AttackInRange)
         {
-            Dictionary<StringName, IEnumerable<Vector2I>> actions = [];
-
             IEnumerable<Vector2I> attackable = unit.AttackableCells(grid, [unit.Cell]);
             IEnumerable<IUnit> targets = grid.GetOccupantUnits().Where((e) => attackable.Contains(e.Key) && !unit.Faction.AlliedTo(e.Value.Faction)).Select((p) => p.Value);
             if (targets.Any())
                 actions["Attack"] = targets.Select((u) => u.Cell);
-
-            return actions;
         }
-        else
-            return [];
+        if (SupportInRange)
+        {
+            IEnumerable<Vector2I> supportable = unit.SupportableCells(grid, [unit.Cell]);
+            IEnumerable<IUnit> targets = grid.GetOccupantUnits().Where((e) => supportable.Contains(e.Key) && unit.Faction.AlliedTo(e.Value.Faction)).Select((p) => p.Value);
+            if (targets.Any())
+                actions["Support"] = targets.Select((u) => u.Cell);
+        }
+
+        return actions;
     }
 }
