ffedf657080a3ae07a571474211bda435a8f91fa
aroelke <alec.roelke@gmail.com>
2025-08-11 13:26:52 -0400

    Add mouse button map

---
+++ src/TbsTemplate/UI/Controls/IconMaps/MouseIconMap.cs
@@ -0,0 +1,12 @@
+using Godot;
+using TbsTemplate.UI.Controls.Device;
+
+namespace TbsTemplate.UI.Controls.IconMaps;
+
+[GlobalClass, Tool]
+public partial class MouseIconMap : GenericIconMap<MouseButton>
+{
+    [Export] public override Godot.Collections.Dictionary<MouseButton, Texture2D> Icons { get; set; } = [];
+    public override MouseButton GetInput(StringName action) => InputManager.GetInputMouseButton(action);
+    public override bool InputIsInvalid(MouseButton input) => input == MouseButton.None;
+}
