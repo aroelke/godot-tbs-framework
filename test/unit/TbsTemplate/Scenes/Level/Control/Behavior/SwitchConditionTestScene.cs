c018d7150abe7186a5537861f24ac253a054bee6
aroelke <alec.roelke@gmail.com>
2025-08-27 17:15:10 -0400

    Add turn switch unit tests

---
+++ test/unit/TbsTemplate/Scenes/Level/Control/Behavior/SwitchConditionTestScene.cs
@@ -0,0 +1,37 @@
+using GD_NET_ScOUT;
+using Godot;
+using TbsTemplate.Scenes.Level.Events;
+using TbsTemplate.Scenes.Level.Object.Group;
+
+namespace TbsTemplate.Scenes.Level.Control.Test;
+
+[Test]
+public partial class SwitchConditionTestScene : Node
+{
+    [Test]
+    public void TestManualSwitchCondition()
+    {
+        ManualSwitchCondition dut = GetNode<ManualSwitchCondition>("ManualSwitchCondition");
+        Assert.IsFalse(dut.Satisfied);
+        dut.Trigger();
+        Assert.IsTrue(dut.Satisfied);
+    }
+
+    private void TestTurnSwitchCondition(int turn, int target, Army army, bool expected)
+    {
+        TurnSwitchCondition dut = GetNode<TurnSwitchCondition>("TurnSwitchCondition");
+        dut.TriggerTurn = target;
+
+        // Reset switch condition
+        LevelEvents.Singleton.EmitSignal(LevelEvents.SignalName.TurnBegan, 1, GetNode<Army>("AllyArmy"));
+
+        // Perform test
+        LevelEvents.Singleton.EmitSignal(LevelEvents.SignalName.TurnBegan, turn, army);
+        Assert.AreEqual(dut.Satisfied, expected);
+    }
+
+    [Test] public void TestTurnSwitchConditionFirstTurn() => TestTurnSwitchCondition(1, 5, GetNode<Army>("AllyArmy"), false);
+    [Test] public void TestTurnSwitchConditionBeforeTrigger() => TestTurnSwitchCondition(4, 5, GetNode<Army>("AllyArmy"), false);
+    [Test] public void TestTurnSwitchConditionWrongArmy() => TestTurnSwitchCondition(5, 5, GetNode<Army>("EnemyArmy"), false);
+    [Test] public void TestTurnSwitchConditionRightArmy() => TestTurnSwitchCondition(5, 5, GetNode<Army>("AllyArmy"), true);
+}
