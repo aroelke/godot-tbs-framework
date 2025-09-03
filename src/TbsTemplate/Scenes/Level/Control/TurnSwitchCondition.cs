b992fd07fe3f97a7dfe04012bd4755e90ce842c4
aroelke <alec.roelke@gmail.com>
2025-09-02 22:35:49 -0400

    Add comments to TurnSwitchCondition

--- src/TbsTemplate/Scenes/Level/Control/TurnSwitchCondition.cs
+++ src/TbsTemplate/Scenes/Level/Control/TurnSwitchCondition.cs
@@ -4,11 +4,14 @@ using TbsTemplate.Scenes.Level.Object.Group;
 
 namespace TbsTemplate.Scenes.Level.Control;
 
+/// <summary>Behavior switch condition that triggers when a particular turn is reached.</summary>
 [Tool]
 public partial class TurnSwitchCondition : SwitchCondition
 {
+    /// <summary>Turn that triggers the behavior switch.</summary>
     [Export(PropertyHint.Expression, "1,10,or_greater")] public int TriggerTurn = 1;
 
+    /// <summary>Army whose turn should be tracked for triggering. Leave <c>null</c> to track all armies.</summary>
     [Export] public Army TriggerArmy = null;
 
     public void Update(int turn, Army army)
