fc6e7446d1559cc41e4df7fdd2316233d7218b7e
aroelke <alec.roelke@gmail.com>
2025-09-03 12:53:28 -0400

    Create unit test scene for SwitchBehavior

--- src/TbsTemplate/Scenes/Level/Control/SwitchBehavior.cs
+++ src/TbsTemplate/Scenes/Level/Control/SwitchBehavior.cs
@@ -49,6 +49,9 @@ public partial class SwitchBehavior : Behavior
 
     public override IEnumerable<Vector2I> Destinations(IUnit unit, IGrid grid) => TargetBehavior()?.Destinations(unit, grid) ?? [];
 
+    /// <summary>Reset the state of the behavior. Mainly intended to be used for testing.</summary>
+    public void Reset() => _switched = false;
+
     public override string[] _GetConfigurationWarnings()
     {
         List<string> warnings = [.. base._GetConfigurationWarnings() ?? []];
---
+++ test/unit/TbsTemplate/Scenes/Level/Control/Behavior/SwitchBehaviorTestScene.cs
@@ -0,0 +1,27 @@
+using GD_NET_ScOUT;
+using Godot;
+
+namespace TbsTemplate.Scenes.Level.Control.Test;
+
+[Test]
+public partial class SwitchBehaviorTestScene : Node
+{
+    private static (Behavior initial, Behavior final) GetBehaviorOptions(SwitchBehavior dut) => (dut.GetNode<Behavior>("BehaviorA"), dut.GetNode<Behavior>("BehaviorB"));
+
+    [Test]
+    public void TestDefaultSwitchBehaviorLatches()
+    {
+        SwitchBehavior dut = GetNode<SwitchBehavior>("DefaultSwitchBehavior");
+        ManualSwitchCondition condition = dut.GetNode<ManualSwitchCondition>("ManualSwitchCondition");
+        (Behavior initial, Behavior final) = GetBehaviorOptions(dut);
+
+        dut.Reset();
+        condition.Reset();
+        Assert.AreEqual(dut.TargetBehavior(), initial);
+
+        condition.Trigger();
+        Assert.AreEqual(dut.TargetBehavior(), final);
+        condition.Trigger();
+        Assert.AreEqual(dut.TargetBehavior(), final);
+    }
+}
