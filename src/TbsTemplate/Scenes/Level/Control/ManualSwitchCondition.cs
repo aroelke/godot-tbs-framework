efede2f5d7b17267bf106bcafc79d664ebda4b45
aroelke <alec.roelke@gmail.com>
2025-09-02 22:35:40 -0400

    Add comments to ManualSwitchCondition

--- src/TbsTemplate/Scenes/Level/Control/ManualSwitchCondition.cs
+++ src/TbsTemplate/Scenes/Level/Control/ManualSwitchCondition.cs
@@ -2,8 +2,10 @@ using Godot;
 
 namespace TbsTemplate.Scenes.Level.Control;
 
+/// <summary>Switch condition that only switches when a function is called.</summary>
 [Tool]
 public partial class ManualSwitchCondition : SwitchCondition
 {
+    /// <summary>Switch whether the condition is satisfied.</summary>
     public void Trigger() => Satisfied = !Satisfied;
 }
