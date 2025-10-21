0a3b583e0954230bb5bdfb7ac41e00cf7fcd6509
aroelke <alec.roelke@gmail.com>
2025-10-21 11:29:54 -0400

    Update SimpleState icon

--- src/TbsTemplate/Nodes/StateChart/States/SimpleState.cs
+++ src/TbsTemplate/Nodes/StateChart/States/SimpleState.cs
@@ -6,13 +6,13 @@ namespace TbsTemplate.Nodes.StateCharts.States;
 
 /// <summary>A <see cref="State"/> that can't have any sub-<see cref="State"/>s.</summary>
 [Icon("res://icons/statechart/AtomicState.svg"), Tool]
-public partial class AtomicState : State
+public partial class SimpleState : State
 {
     public override void HandleTransition(Transition transition, State from) => GetParent<State>().HandleTransition(transition, from);
 
     public override string[] _GetConfigurationWarnings()
     {
-        List<string> warnings = new(base._GetConfigurationWarnings() ?? []);
+        List<string> warnings = [.. base._GetConfigurationWarnings() ?? []];
 
         if (GetChildren().OfType<State>().Any())
             warnings.Add("Child states of atomic states can't be transitioned to.");
