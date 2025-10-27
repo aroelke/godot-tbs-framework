a2851edfb1d3827f3cf30f4073492a75b878538a
aroelke <alec.roelke@gmail.com>
2025-10-27 11:59:47 -0400

    Add scene loaders to Class

--- src/TbsTemplate/Data/Class.cs
+++ src/TbsTemplate/Data/Class.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Godot;
 using TbsTemplate.Extensions;
 
@@ -7,28 +11,117 @@ namespace TbsTemplate.Data;
 [GlobalClass, Tool]
 public partial class Class : Resource
 {
+    private class PackedSceneMap(IDictionary<Faction, string> paths, IDictionary<Faction, PackedScene> scenes, Func<Faction, PackedScene> loader) : IReadOnlyDictionary<Faction, PackedScene>
+    {
+        private void LoadAllScenes()
+        {
+            foreach (Faction faction in paths.Keys)
+                loader(faction);
+        }
+
+        public PackedScene this[Faction key] => loader(key);
+
+        public IEnumerable<Faction> Keys => paths.Keys;
+
+        public IEnumerable<PackedScene> Values
+        {
+            get
+            {
+                LoadAllScenes();
+                return scenes.Values;
+            }
+        }
+
+        public int Count => paths.Count;
+
+        public bool ContainsKey(Faction key) => paths.ContainsKey(key);
+
+        public IEnumerator<KeyValuePair<Faction, PackedScene>> GetEnumerator()
+        {
+            LoadAllScenes();
+            return scenes.GetEnumerator();
+        }
+
+        public bool TryGetValue(Faction key, [MaybeNullWhen(false)] out PackedScene value)
+        {
+            if (!paths.ContainsKey(key))
+            {
+                value = null;
+                return false;
+            }
+            else
+            {
+                value = loader(key);
+                return true;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    private static PackedScene LoadAnimations(Faction faction, IDictionary<Faction, string> paths, IDictionary<Faction, PackedScene> scenes)
+    {
+        if (paths.TryGetValue(faction, out string path))
+        {
+            if (scenes.TryGetValue(faction, out PackedScene scene))
+                return scene;
+            else
+                return scenes[faction] = ResourceLoader.Load<PackedScene>(path);
+        }
+        else
+            throw new ArgumentException($"Faction {faction.Name} does not have any map animations defined.");
+    }
+
+    private readonly Dictionary<Faction, PackedScene> _mapAnimations = [];
+    private PackedScene _defaultMapAnimations = null;
+    private readonly Dictionary<Faction, PackedScene> _combatAnimations = [];
+    private PackedScene _defaultCombatAnimations = null;
     private PackedScene _combatAnimation = null;
 
     /// <summary>Loaded (but not instantiated) combat animation scene for the class.</summary>
     public PackedScene CombatAnimations => LoadCombatAnimations();
 
-    /// <summary>Mapping of factions onto respective scenes defining map animations for units of this class.</summary>
+    /// <summary>Mapping of factions onto respective paths to scenes defining map animations for units of this class.</summary>
     public Godot.Collections.Dictionary<Faction, string> MapAnimationsPaths = [];
+
+    /// <summary>
+    /// Mapping of factions onto respective scenes defining map animations for units of this class. Scenes are loaded from <see cref="MapAnimationsPaths"/>
+    /// the first time each faction's is accessed.
+    /// </summary>
+    public IReadOnlyDictionary<Faction, PackedScene> MapAnimationsScenes { get; private set; } = null;
+
+    /// <summary>
+    /// Path to default map animation scene for units of this class if their <see cref="Faction"/> isn't present in <see cref="MapAnimationsPaths"/>.
+    /// Colorize using <see cref="Faction.Color"/> to differentiate.
+    /// </summary>
+    public string DefaultMapAnimationsPath = "";
 
     /// <summary>
     /// Default map animation scene for units of this class if their <see cref="Faction"/> isn't present in <see cref="MapAnimationsPaths"/>.
     /// Colorize using <see cref="Faction.Color"/> to differentiate.
     /// </summary>
-    public string DefaultMapAnimationsPath = "";
+    public PackedScene DefaultMapAnimationsScene => LoadDefaultMapAnimations();
 
-    /// <summary>Mapping of factions on to respective scenes defining combat animations for units of this class.</summary>
+    /// <summary>Mapping of factions on to respective paths to scenes defining combat animations for units of this class.</summary>
     public Godot.Collections.Dictionary<Faction, string> CombatAnimationsPaths = [];
+
+    /// <summary>
+    /// Mapping of factions onto respective scenes defining combat animations for units of this class. Scenes are loaded from <see cref="CombatAnimationsPaths"/>
+    /// the first time each faction's is accessed.
+    /// </summary>
+    public IReadOnlyDictionary<Faction, PackedScene> CombatAnimationsScenes { get; private set; } = null;
+
+    /// <summary>
+    /// Path to default combat animation for units of this class if their <see cref="Faction"/> isn't present in <see cref="CombatAnimationsPaths"/>.
+    /// Colorize using <see cref="Faction.Color"/> to differentiate.
+    /// </summary>
+    public string DefaultCombatAnimationsPath = "";
 
     /// <summary>
     /// Default combat animation for units of this class if their <see cref="Faction"/> isn't present in <see cref="CombatAnimationsPaths"/>.
     /// Colorize using <see cref="Faction.Color"/> to differentiate.
     /// </summary>
-    public string DefaultCombatAnimationsPath = "";
+    public PackedScene DefaultCombatAnimationsScene => LoadDefaultCombatAnimations();
 
     /// <summary>Mapping of factions onto sprites to display for units of this class in the editor.</summary>
     [Export] public Godot.Collections.Dictionary<Faction, Texture2D> EditorSprites = [];
@@ -41,6 +134,30 @@ public partial class Class : Resource
 
     /// <summary>Path to the scene containing combat animations for the class.</summary>
     [Export(PropertyHint.File, "*.tscn")] public string CombatAnimationPath = "";
+
+    public Class() : base()
+    {
+        MapAnimationsScenes = new PackedSceneMap(MapAnimationsPaths, _mapAnimations, LoadMapAnimations);
+        CombatAnimationsScenes = new PackedSceneMap(CombatAnimationsPaths, _combatAnimations, LoadCombatAnimations);
+    }
+
+    /// <summary>Manually load map animations for units of this class belonging to a faction.</summary>
+    /// <param name="faction">Faction whose animations should be loaded.</param>
+    /// <returns>Scene defining map animations.</returns>
+    public PackedScene LoadMapAnimations(Faction faction) => LoadAnimations(faction, MapAnimationsPaths, _mapAnimations);
+
+    /// <summary>Manually load the default map animation scene for units of this class.</summary>
+    /// <returns>The loaded animation scene.</returns>
+    public PackedScene LoadDefaultMapAnimations() => _defaultMapAnimations ??= ResourceLoader.Load<PackedScene>(DefaultMapAnimationsPath);
+
+    /// <summary>Manually load combat animations for units of this class belonging to a faction.</summary>
+    /// <param name="faction">Faction whose animations should be loaded.</param>
+    /// <returns>Scene defining combat animations.</returns>
+    public PackedScene LoadCombatAnimations(Faction faction) => LoadAnimations(faction, CombatAnimationsPaths, _combatAnimations);
+
+    /// <summary>Manually load the default combat animation scene for units of this class.</summary>
+    /// <returns>The loaded animation scene.</returns>
+    public PackedScene LoadDefaultCombatAnimations() => _defaultCombatAnimations ??= ResourceLoader.Load<PackedScene>(DefaultCombatAnimationsPath);
 
     /// <summary>Manually load the combat animation scene for control over when it gets loaded into memory.</summary>
     /// <returns>The loaded animation scene.</returns>
