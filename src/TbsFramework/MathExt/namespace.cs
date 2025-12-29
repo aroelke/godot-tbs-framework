852b30baab6a13e1920e7e1eaa5b21668d91cc4e
aroelke <alec.roelke@gmail.com>
2025-12-29 18:27:39 -0500

    Remove unused collection functions

--- src/TbsFramework/MathExt/namespace.cs
+++
@@ -1,8 +0,0 @@
-namespace TbsFramework
-{
-    namespace MathExt
-    {
-        /// <summary>Methods for performing math and collection operations in addition to <see cref="System.Math"/>.</summary>
-        static internal class NamespaceDoc {}
-    }
-}
