ddd1bf5b578a461e10c8c8f7b9f7fc7e25f5287f
aroelke <alec.roelke@gmail.com>
2026-01-02 19:27:01 -0500

    Give GridObjectData its own copy constructor
    for subclasses to use

--- src/TbsFramework/Scenes/Level/Object/GridObjectData.cs
+++ src/TbsFramework/Scenes/Level/Object/GridObjectData.cs
@@ -8,6 +8,7 @@ public abstract class GridObjectData(bool occupies)
 {
     public delegate void CellChangedEventHandler(Vector2I cell);
 
+    private readonly bool _occupant = occupies;
     private GridData _grid = null;
     private Vector2I _cell = -Vector2I.One;
 
@@ -20,6 +21,7 @@ public abstract class GridObjectData(bool occupies)
         {
             if (_grid != value)
             {
+                _grid?.Occupants.Remove(_cell);
                 _grid = value;
                 if (_grid is not null)
                 {
@@ -38,11 +40,11 @@ public abstract class GridObjectData(bool occupies)
             Vector2I next = _grid?.Clamp(value) ?? value;
             if (_cell != next)
             {
-                if (occupies && (_grid?.Occupants.TryGetValue(next, out GridObjectData occupant) ?? false) && occupant != this)
+                if (_occupant && (_grid?.Occupants.TryGetValue(next, out GridObjectData occupant) ?? false) && occupant != this)
                     throw new ArgumentException($"Cell {next} is already occupied");
                 Vector2I old = _cell;
                 _cell = next;
-                if (occupies && _grid is not null)
+                if (_occupant && _grid is not null)
                 {
                     _grid.Occupants.Remove(old);
                     _grid.Occupants[_cell] = this;
@@ -51,6 +53,12 @@ public abstract class GridObjectData(bool occupies)
                     CellChanged(_cell);
             }
         }
+    }
+
+    protected GridObjectData(GridObjectData original) : this(original._occupant)
+    {
+        _cell = original.Cell;
+        // Leave _grid null because it already has an occupant in that spot
     }
 
     public abstract GridObjectData Clone();
