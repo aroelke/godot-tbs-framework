101e2e8e485bf1f04d82db9eb384062d7d3b520b
aroelke <alec.roelke@gmail.com>
2026-02-04 17:54:51 -0500

    Create namespace for renderers

---
+++ src/TbsFramework/Rendering/namespace.cs
@@ -0,0 +1,8 @@
+namespace TbsFramework
+{
+    namespace Rendering
+    {
+        /// <summary>Objects used to render the state of the game and provide information about the screen.</summary>
+        static internal class NamespaceDoc {}
+    }
+}
