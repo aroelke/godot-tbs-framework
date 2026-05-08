ca4f84d40cc8d915d6dcef07335e5f03d16879f5
aroelke <alec.roelke@gmail.com>
2026-05-08 13:43:31 -0400

    Add digital controls to pause screen

--- demo/DemoPauseOverlay.cs
+++ demo/DemoPauseOverlay.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Godot;
 using TbsFramework.UI.Controls.Device;
 
@@ -7,10 +8,14 @@ public partial class DemoPauseOverlay : Control
 
     [Signal] public delegate void GameResumedEventHandler();
 
+    private int _selected = 0;
+    private Button[] _buttons = null;
+
     public void Pause()
     {
         GetTree().Paused = true;
         Visible = true;
+        _buttons[_selected = 0].GrabFocus();
         EmitSignal(SignalName.GamePaused);
     }
 
@@ -23,6 +28,14 @@ public partial class DemoPauseOverlay : Control
         Visible = false;
         GetTree().Paused = false;
         EmitSignal(SignalName.GameResumed);
+    }
+
+    public void OnDirectionPressed(Vector2I direction) => _buttons[_selected = (_selected + direction.Y) % _buttons.Length].GrabFocus();
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _buttons = [.. GetNode("Buttons").GetChildren().OfType<Button>()];
     }
 
     public override void _Input(InputEvent @event)
