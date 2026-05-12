e6cc542c39aeb00ff0a46b4a2f836f0c0ac1a3a6
aroelke <alec.roelke@gmail.com>
2026-05-11 22:39:13 -0400

    Add sounds/button controls to game over screen

--- demo/DemoGameOverScene.cs
+++ demo/DemoGameOverScene.cs
@@ -6,13 +6,20 @@ namespace TbsFramework.Demo;
 /// <summary>Simple demo game over screen that notifies the player whether they won or lost.</summary>
 public partial class DemoGameOverScene : Node
 {
+    private static readonly StringName RestartItem = "Restart";
+    private static readonly StringName QuitItem = "Quit";
+
     [Export] public bool win = true;
 
     [Export(PropertyHint.File, "*.tscn")] public string RestartTarget = null;
 
-    public void RestartGame() => SceneManager.CallScene(RestartTarget);
-
-    public void EndGame() => GetTree().Quit();
+    public void OnItemSelected(StringName item)
+    {
+        if (item == RestartItem)
+            SceneManager.CallScene(RestartTarget);
+        else if (item == QuitItem)
+            GetTree().Quit();
+    }
 
     public override void _Ready()
     {
