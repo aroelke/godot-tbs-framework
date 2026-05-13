d88a218ac47a3bee4c9872b89c15307a165a98d5
aroelke <alec.roelke@gmail.com>
2026-05-12 22:58:19 -0400

    Add some more properties to TimeObjective
    to allow it to trigger at the end of the round

--- src/TbsFramework/Scenes/Level/Objectives/TimeObjective.cs
+++ src/TbsFramework/Scenes/Level/Objectives/TimeObjective.cs
@@ -8,27 +8,57 @@ namespace TbsFramework.Scenes.Level.Objectives;
 [Tool]
 public partial class TimeObjective : Objective
 {
-    private int _turn = 0;
+    /// <summary>Possible turn events that can trigger the time objective to update and potentially become completed.</summary>
+    public enum TriggerEvent { TurnBegan, TurnEnded }
 
-    private void OnTurnBegan(int turn, Faction _) => _turn = turn;
+    private int _turn = 0;
 
     /// <summary>Number of turns to elapse before completion.</summary>
     [Export(PropertyHint.Range, "1,10,or_greater")] public int Turns = 0;
 
-    public override bool Complete => _turn > Turns;
+    /// <summary>Army whose turn count is to be tracked. Leave <c>null</c> to track all armies.</summary>
+    [Export] public Army Army = null;
+
+    /// <summary>When to update the turn counter.</summary>
+    [Export] public TriggerEvent Trigger = TriggerEvent.TurnBegan;
+
+    public override bool Complete => Trigger switch {
+        TriggerEvent.TurnBegan => _turn > Turns,
+        TriggerEvent.TurnEnded => _turn >= Turns,
+        _ => false
+    };
     public override string Description => $"Survive {Turns} Turns";
+
+    /// <summary>Update the turn count if it's the right event for the right army's turn.</summary>
+    /// <param name="turn">Turn to update to.</param>
+    /// <param name="faction">Faction whose turn it should be to update.</param>
+    /// <param name="trigger">Turn event to update the turn count on.</param>
+    public void UpdateTurnCount(int turn, Faction faction, TriggerEvent trigger)
+    {
+        if (Trigger == trigger && (Army is null || Army.Faction == faction))
+            _turn = turn;
+    }
+
+    public void OnTurnBegan(int turn, Faction faction) => UpdateTurnCount(turn, faction, TriggerEvent.TurnBegan);
+    public void OnTurnEnded(int turn, Faction faction) => UpdateTurnCount(turn, faction, TriggerEvent.TurnEnded);
 
     public override void _EnterTree()
     {
         base._EnterTree();
         if (!Engine.IsEditorHint())
+        {
             LevelEvents.TurnBegan += OnTurnBegan;
+            LevelEvents.TurnEnded += OnTurnEnded;
+        }
     }
 
     public override void _ExitTree()
     {
         base._ExitTree();
         if (!Engine.IsEditorHint())
+        {
             LevelEvents.TurnBegan -= OnTurnBegan;
+            LevelEvents.TurnEnded -= OnTurnEnded;
+        }
     }
 }
