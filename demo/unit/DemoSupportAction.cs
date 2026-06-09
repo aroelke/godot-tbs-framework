320518cfea0cafa119cfce06d57a44ec6cbdd60a
aroelke <alec.roelke@gmail.com>
2026-06-08 23:17:19 -0400

    Attack/Support actions both need to return lists

--- demo/unit/DemoSupportAction.cs
+++ demo/unit/DemoSupportAction.cs
@@ -8,7 +8,7 @@ using TbsFramework.Scenes.Data;
 
 namespace TbsFramework.Demo;
 
-public class DemoSupportAction : IUnitAction<CombatAction>
+public class DemoSupportAction : IUnitAction<List<CombatAction>>
 {
     public bool CanPerform(UnitData unit) => unit.Stats.Healing > 0;
 
@@ -20,11 +20,11 @@ public class DemoSupportAction : IUnitAction<CombatAction>
 
     public IEnumerable<Vector2I> GetTargetCells(UnitData unit, Vector2I cell) => unit.GetSupportableCells(cell);
 
-    public CombatAction Perform(UnitData unit, Vector2I target)
+    public List<CombatAction> Perform(UnitData unit, Vector2I target)
     {
         if (!unit.Grid.Occupants.TryGetValue(target, out UnitData occupant))
             throw new ArgumentException($"Cell {target} does not contain a unit to attack");
-        return CombatCalculations.CreateSupportAction(unit, occupant);
+        return [CombatCalculations.CreateSupportAction(unit, occupant)];
     }
 
     public GridData Simulate(UnitData unit, Vector2I source, Vector2I target)
@@ -33,15 +33,15 @@ public class DemoSupportAction : IUnitAction<CombatAction>
             throw new ArgumentException($"Cell {target} does not contain a unit to attack");
         CombatAction action = CombatCalculations.CreateSupportAction(unit, occupant);
         GridData copy = unit.Grid.Clone();
-        UpdateGrid(copy, action);
+        UpdateGrid(copy, [action]);
         return copy;
     }
 
-    public void UpdateGrid(GridData grid, CombatAction results)
+    public void UpdateGrid(GridData grid, List<CombatAction> results)
     {
         // Get the version of the action's actor on the input grid to avoid updating the wrong grid
-        UnitData target = grid.Occupants[results.Target.Cell];
+        UnitData target = grid.Occupants[results[0].Target.Cell];
         // Remember that the amount of healing from a healing action is stored as negative damage
-        target.Health -= results.Damage;
+        target.Health -= results[0].Damage;
     }
 }
