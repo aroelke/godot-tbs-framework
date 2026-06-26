13625a2514b55f49c7ccfb748b6fc2ae550aca07
aroelke <alec.roelke@gmail.com>
2026-06-26 18:43:31 -0400

    Initial implementation of new UnitAction behavior

--- src/TbsFramework/Scenes/Level/Actions/ActionRange.cs
+++ src/TbsFramework/Scenes/Level/Actions/ActionRange.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using TbsFramework.Scenes.Data;
 using TbsFramework.Scenes.Level.Events;
@@ -10,6 +11,8 @@ public abstract partial class ActionRange : Resource
     public abstract IEnumerable<Vector2I> GetAllCellsInRange(UnitData unit, Vector2I cell);
 
     public abstract IEnumerable<Vector2I> GetValidCellsInRange(UnitData unit, Vector2I cell);
+
+    public virtual bool InRange(UnitData unit, Vector2I source, Vector2I target) => GetValidCellsInRange(unit, source).Contains(target);
 
     public virtual void Initialize(LevelManager manager) {}
 }
--- src/TbsFramework/Scenes/Level/Actions/UnitAction.cs
+++ src/TbsFramework/Scenes/Level/Actions/UnitAction.cs
@@ -1,17 +1,96 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
+using TbsFramework.Scenes.Data;
+using TbsFramework.Scenes.Level.Control;
 using TbsFramework.Scenes.Level.Events;
 
 namespace TbsFramework.Scenes.Level.Actions;
 
 public partial class UnitAction : Resource
 {
+    [Export] public bool IntersectPermission = false;
+
     [Export] public Godot.Collections.Array<ActionPermission> PermissionComponents = [];
 
+    [Export] public bool IntersectDomains = false;
+
     [Export] public Godot.Collections.Array<ActionDomain> DomainComponents = [];
+
+    [Export] public bool IntersectRanges = false;
 
     [Export] public Godot.Collections.Array<ActionRange> RangeComponents = [];
 
     [Export] public ActionExecute ExecuteComponent = null;
+
+    public bool CanPerform(UnitData unit, Vector2I source, Vector2I target)
+    {
+        bool hasPermission = IntersectPermission ? PermissionComponents.All((c) => c.CanPerform(unit)) : PermissionComponents.Any((c) => c.CanPerform(unit));
+        bool inDomain = IntersectDomains ? DomainComponents.All((c) => c.Contains(source)) : DomainComponents.Any((c) => c.Contains(source));
+        bool inRange = RangeComponents.Count == 0 || (IntersectRanges ? RangeComponents.All((c) => c.InRange(unit, source, target)) : RangeComponents.Any((c) => c.InRange(unit, source, target)));
+        return hasPermission && inDomain && inRange;
+    }
+
+    /// <summary>
+    /// Get the cells a unit can perform this action on from a specific cell.
+    /// </summary>
+    /// <param name="unit"></param>
+    /// <param name="cell"></param>
+    /// <returns></returns>
+    public IEnumerable<Vector2I> GetTargetCells(UnitData unit, Vector2I cell)
+    {
+        IEnumerable<HashSet<Vector2I>> ranges = RangeComponents.Select((c) => c.GetValidCellsInRange(unit, cell).ToHashSet());
+        return ranges.Aggregate(IntersectRanges ? (a, b) => a.Intersect(b).ToHashSet() : (a, b) => a.Union(b).ToHashSet());
+    }
+
+    /// <summary>Get all cells a unit can reach to perform this action on from any cell it can move to from its current location for display on the map.</summary>
+    /// <param name="unit"></param>
+    /// <returns></returns>
+    /// <remarks>It is up to the implementor to determine if cells that are in reach but not valid targets should be included.</remarks>
+    public IEnumerable<Vector2I> GetAllTargetCells(UnitData unit)
+    {
+        IEnumerable<HashSet<Vector2I>> ranges = RangeComponents.Select((r) => unit.GetTraversableCells().SelectMany((c) => r.GetAllCellsInRange(unit, c)).ToHashSet());
+        return ranges.Aggregate(IntersectRanges ? (a, b) => a.Intersect(b).ToHashSet() : (a, b) => a.Union(b).ToHashSet());
+    }
+
+    /// <summary>
+    /// Get the cells a unit can perform this action on from any cell it can move to from its current location.
+    /// </summary>
+    /// <param name="unit"></param>
+    /// <returns></returns>
+    public IEnumerable<Vector2I> GetValidTargetCells(UnitData unit)
+    {
+        IEnumerable<HashSet<Vector2I>> ranges = RangeComponents.Select((r) => unit.GetTraversableCells().SelectMany((c) => r.GetValidCellsInRange(unit, c)).ToHashSet());
+        return ranges.Aggregate(IntersectRanges ? (a, b) => a.Intersect(b).ToHashSet() : (a, b) => a.Union(b).ToHashSet());
+    }
+
+    /// <summary>
+    /// Compute the result of performing this action.
+    /// </summary>
+    /// <param name="unit"></param>
+    /// <param name="target"></param>
+    /// <returns>A data structure representing the result of <paramref name="unit"/> performing this action on cell <paramref name="target"/>.</returns>
+    /// <exception cref="ArgumentException">If <paramref name="target"/> is not a valid target cell to perform this action on.</exception>
+    public UnitActionResult Perform(UnitData unit, Vector2I target) => ExecuteComponent.Perform(unit, target);
+
+    /// <summary>
+    /// Update a grid with the results of this action as computed by <see cref="Perform(UnitData, Vector2I)"/>.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="result"></param>
+    /// <exception cref="ArgumentException">If <paramref name="result"/>.Result contains invalid data for performing this action.</exception>
+    public void UpdateGrid(GridData grid, UnitActionResult result) => ExecuteComponent.UpdateGrid(grid, result);
+
+    /// <summary>
+    /// Simulate the results of this action, resolving any nondeterminism in some nonrandom way (such as by averaging possible results). Makes no changes
+    /// to the state of any existing grid.
+    /// </summary>
+    /// <param name="unit"></param>
+    /// <param name="source"></param>
+    /// <param name="target"></param>
+    /// <returns>A new grid containing the result of the simulation of performing this action.</returns>
+    /// <remarks><b>Note</b>: This is intended for use by <see cref="AIController"/> to evaluate actions.</remarks>
+    public GridData Simulate(UnitData unit, Vector2I source, Vector2I target) => ExecuteComponent.Simulate(unit, source, target);
 
     public void Initialize(LevelManager manager)
     {
