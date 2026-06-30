9e26c45e4c54604ce67d0d75a95c12ed0f34691b
aroelke <alec.roelke@gmail.com>
2026-06-29 22:33:41 -0400

    Add comments to ActionPermissionFaction

--- src/TbsFramework/Scenes/Level/Actions/ActionPermissionFaction.cs
+++ src/TbsFramework/Scenes/Level/Actions/ActionPermissionFaction.cs
@@ -4,9 +4,11 @@ using TbsFramework.Scenes.Data;
 
 namespace TbsFramework.Scenes.Level.Actions;
 
+/// <summary>Gives permission to a unit to perform an action based on the <see cref="Faction"/> it belongs to.</summary>
 [GlobalClass, Tool]
 public partial class ActionPermissionFaction : ActionPermission
 {
+    /// <summary>List of factions allowed to perform an action. Leave empty to allow all factions to perform it.</summary>
     [Export] public Faction[] AllowedFactions = [];
 
     public override bool CanPerform(UnitData unit) => AllowedFactions.Length == 0 || AllowedFactions.Contains(unit.Faction);
