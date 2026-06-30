b76e76caa8fcb13177120f494452e07d668d5770
aroelke <alec.roelke@gmail.com>
2026-06-29 22:30:55 -0400

    Add comments to ActionExecuteRegion

--- src/TbsFramework/Scenes/Level/Actions/ActionExecuteRegion.cs
+++ src/TbsFramework/Scenes/Level/Actions/ActionExecuteRegion.cs
@@ -5,11 +5,14 @@ using TbsFramework.Scenes.Rendering;
 
 namespace TbsFramework.Scenes.Level.Actions;
 
+/// <summary>Defines the execution of an action to be to update a <see cref="SpecialActionRegion"/> with the unit performing the action.</summary>
 [GlobalClass, Tool]
 public partial class ActionExecuteRegion : ActionExecute
 {
+    /// <summary>Scene path to the <see cref="SpecialActionRegion"/> relative to the parameter of <see cref="Initialize"/>.</summary>
     [Export(PropertyHint.NodePathValidTypes, nameof(SpecialActionRegion))] public NodePath RegionPath = null;
 
+    /// <summary>Node resolved from <see cref="RegionPath"/> after <see cref="Initialize"/> completes.</summary>
     public SpecialActionRegion Region = null;
 
     public override object Perform(UnitData unit, Vector2I target) => null;
