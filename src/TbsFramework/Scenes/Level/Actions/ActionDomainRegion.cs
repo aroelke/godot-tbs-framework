256b76eb81b73e42ff22ec2f0b7f94b742bde2aa
aroelke <alec.roelke@gmail.com>
2026-06-29 22:28:26 -0400

    Add comments to ActionDomainRegion

--- src/TbsFramework/Scenes/Level/Actions/ActionDomainRegion.cs
+++ src/TbsFramework/Scenes/Level/Actions/ActionDomainRegion.cs
@@ -4,11 +4,14 @@ using TbsFramework.Scenes.Rendering;
 
 namespace TbsFramework.Scenes.Level.Actions;
 
+/// <summary>Defines the cells a unit can perform an action from using the used cells of a <see cref="SpecialActionRegion"/>.</summary>
 [GlobalClass, Tool]
 public partial class ActionDomainRegion : ActionDomain
 {
+    /// <summary>Scene path to the <see cref="SpecialActionRegion"/> relative to the parameter of <see cref="Initialize"/>.</summary>
     [Export(PropertyHint.NodePathValidTypes, nameof(SpecialActionRegion))] public NodePath RegionPath = null;
 
+    /// <summary>Node resolved from <see cref="RegionPath"/> after <see cref="Initialize"/> completes.</summary>
     public SpecialActionRegion Region = null;
 
     public override bool Contains(Vector2I cell) => Region.Data.Cells.Contains(cell);
