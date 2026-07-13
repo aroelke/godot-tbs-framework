4370589790f59ef79ea031f97666002bc59b2375
aroelke <alec.roelke@gmail.com>
2026-07-13 17:02:21 -0400

    Only compare ally health for support actions

--- src/TbsFramework/Scenes/Level/Control/AIController.cs
+++ src/TbsFramework/Scenes/Level/Control/AIController.cs
@@ -134,13 +134,10 @@ public partial class AIController : ArmyController
             if ((diff = other.DefeatedAllies - DefeatedAllies) != 0)
                 return diff;
 
-            if (Action.RequiresTarget && other.Action.RequiresTarget && Result.Occupants[Target].Faction.AlliedTo(Actor.Faction) == other.Result.Occupants[other.Target].Faction.AlliedTo(other.Actor.Faction))
+            if (Action.RequiresTarget && other.Action.RequiresTarget && Result.Occupants[Target].Faction.AlliedTo(Actor.Faction) && other.Result.Occupants[other.Target].Faction.AlliedTo(other.Actor.Faction))
             {
-                if (Result.Occupants[Target].Faction.AlliedTo(Actor.Faction) &&
-                    other.Result.Occupants[other.Target].Faction.AlliedTo(other.Actor.Faction) &&
-                    (diff = (int)(other.Actor.Grid.Occupants[other.Target].Health - Actor.Grid.Occupants[Target].Health)) != 0)
+                if ((diff = (int)(other.Actor.Grid.Occupants[other.Target].Health - Actor.Grid.Occupants[Target].Health)) != 0)
                     return diff;
-                GD.Print($"health diff: {other.AllyHealthDifference} {AllyHealthDifference}");
                 if ((diff = (int)((other.AllyHealthDifference - AllyHealthDifference)*HealthDiffPrecision)) != 0)
                     return diff;
             }
