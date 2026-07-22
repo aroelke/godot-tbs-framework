ddcac1ce6a608519e657034f4f520fae733b5540
aroelke <alec.roelke@gmail.com>
2026-07-22 17:50:52 -0400

    Set SwitchCondition to use an action for region

--- src/TbsFramework/Scenes/Level/Control/InRangeSwitchCondition.cs
+++ src/TbsFramework/Scenes/Level/Control/InRangeSwitchCondition.cs
@@ -1,14 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
+using TbsFramework.Scenes.Level.Actions;
 using TbsFramework.Scenes.Rendering;
 
 namespace TbsFramework.Scenes.Level.Control;
 
-/// <summary>Behavior switch condition that triggers based on units being in attack range of other units.</summary>
+/// <summary>Behavior switch condition that triggers based on units being in range to perform an action on other units.</summary>
 [Tool]
 public partial class InRangeSwitchCondition : AreaSwitchCondition
 {
+    /// <summary>Action whose range determines the region that controls the behavior switch.</summary>
+    [Export] public UnitAction Action = null;
+
     /// <summary>Set of units explicitly used to determine the attack range.</summary>
     [Export] public Unit[] SourceUnits = [];
 
@@ -20,7 +24,7 @@ public partial class InRangeSwitchCondition : AreaSwitchCondition
         List<Unit> sources = [.. SourceUnits.Where(IsInstanceValid)];
         foreach (Army army in SourceArmies)
             sources.AddRange(army);
-        return [.. sources.SelectMany((u) => u.UnitData.GetFilteredAttackableCellsInReach())];
+        return Action is null ? [] : [.. sources.SelectMany((u) => Action.GetValidTargetCells(u.UnitData))];
     }
 
     public override string[] _GetConfigurationWarnings()
@@ -28,7 +32,9 @@ public partial class InRangeSwitchCondition : AreaSwitchCondition
         List<string> warnings = [.. base._GetConfigurationWarnings() ?? []];
 
         if (SourceUnits.Length == 0 && SourceArmies.Length == 0)
-            warnings.Add("No source units have been defined.  There will be no range for trigger units to enter and cause a behavior switch.");
+            warnings.Add("No source units have been defined. There will be no range for trigger units to enter and cause a behavior switch.");
+        if (Action is null)
+            warnings.Add("No action defined to determine the range for. This will prevent the behavior from switching.");
 
         return [.. warnings];
     }
