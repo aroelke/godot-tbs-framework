919f457de7ef72a2bc4a276ca73c7aa45ab6ce69
aroelke <alec.roelke@gmail.com>
2026-07-22 22:37:01 -0400

    Move attack range calculation to demo action

--- demo/actions/DemoRangeAttack.cs
+++ demo/actions/DemoRangeAttack.cs
@@ -12,10 +12,10 @@ public partial class DemoRangeAttack : ActionRange
 {
     public override bool InRange(UnitData unit, Vector2I source, Vector2I target) => unit.Stats.AttackRange.Contains(source.ManhattanDistanceTo(target));
 
-    public override IEnumerable<Vector2I> GetAllCellsInRange(UnitData unit, Vector2I cell) => unit.GetAttackableCells(cell);
+    public override IEnumerable<Vector2I> GetAllCellsInRange(UnitData unit, Vector2I cell) => unit.Grid.GetCellsInRange(cell, unit.Stats.AttackRange);
 
     public override IEnumerable<Vector2I> GetValidCellsInRange(UnitData unit, Vector2I cell) =>
         GetAllCellsInRange(unit, cell).Where((c) => unit.Grid.Occupants.TryGetValue(c, out UnitData occupant) && !unit.Faction.AlliedTo(occupant.Faction));
 
-    public override IEnumerable<Vector2I> GetSources(UnitData unit, Vector2I target) => unit.GetAttackableCells(target);
+    public override IEnumerable<Vector2I> GetSources(UnitData unit, Vector2I target) => unit.Grid.GetCellsInRange(target, unit.Stats.AttackRange);
 }
