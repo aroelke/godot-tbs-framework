9e18754fcc60d7484883a55611493ef7da6ec8fc
aroelke <alec.roelke@gmail.com>
2026-08-19 13:30:18 -0400

    Add comments to PathEvaluator

--- src/TbsFramework/Scenes/Level/Control/PathEvaluator.cs
+++ src/TbsFramework/Scenes/Level/Control/PathEvaluator.cs
@@ -2,9 +2,11 @@ using Godot;
 
 namespace TbsFramework.Scenes.Level.Control;
 
+/// <summary>Evaluates an action based on the length of the path the actor took to get to its destination.</summary>
 [GlobalClass, Tool]
 public partial class PathEvaluator : ActionEvaluator
 {
+    /// <summary><c>true</c> if shorter paths should be evaluated higher and <c>false</c> if longer paths should be higher.</summary>
     [Export] public bool PreferShorter = true;
 
     public override double Evaluate(SimulatedAction action)
