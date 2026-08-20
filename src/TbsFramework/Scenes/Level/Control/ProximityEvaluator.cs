9645ad846fe4eb435ea8911bdd4298c5252541a8
aroelke <alec.roelke@gmail.com>
2026-08-19 22:40:59 -0400

    Improve ProximityEvaluator docs
    - Add comments
    - Make Factions/Units properties disappear when both allies/enemies
      are checked

--- src/TbsFramework/Scenes/Level/Control/ProximityEvaluator.cs
+++ src/TbsFramework/Scenes/Level/Control/ProximityEvaluator.cs
@@ -7,39 +7,84 @@ using TbsFramework.Scenes.Data;
 
 namespace TbsFramework.Scenes.Level.Control;
 
+/// <summary>Strategy to use when making a decision based on distance between two cells.</summary>
 public enum DistanceStrategy
 {
+    /// <summary>Use the Manhattan distance, or the sum of the differences between cell coordinates.</summary>
     ManhattanDistance,
+    /// <summary>Use the number of cells contained in the shortest path between the two cells (accounting for cell cost).</summary>
     PathLength,
+    /// <summary>Use the cost of moving along the shortest path between the two cells.</summary>
     PathCost
 }
 
+/// <summary>Evaluates an action based on the actor's proximity to the unit in a specified group that's closest to it.</summary>
 [GlobalClass, Tool]
 public partial class ProximityEvaluator : ActionEvaluator
 {
-    [Export] public UnitIdentity[] SpecificUnits = [];
+    private bool _allies = false, _enemies = false;
 
-    [Export] public Faction[] InFactions = [];
+    /// <summary>Measure proximity to any allied unit.</summary>
+    [Export] public bool AllAllies
+    {
+        get => _allies;
+        set
+        {
+            if (Engine.IsEditorHint())
+            {
+                if (_allies != value)
+                {
+                    _allies = value;
+                    NotifyPropertyListChanged();
+                }
+            }
+            else
+                _allies = value;
+        }
+    }
 
-    [Export] public bool AnyAlly = false;
+    /// <summary>Measure proximity to any enemy unit.</summary>
+    [Export] public bool AllEnemies
+    {
+        get => _enemies;
+        set
+        {
+            if (Engine.IsEditorHint())
+            {
+                if (_enemies != value)
+                {
+                    _enemies = value;
+                    NotifyPropertyListChanged();
+                }
+            }
+            else
+                _enemies = value;
+        }
+    }
 
-    [Export] public bool AnyEnemy = false;
+    /// <summary>Specific units that are part of the group to measure proximity to.</summary>
+    [Export] public UnitIdentity[] Units = [];
 
+    /// <summary>Factions containing units that are part of the group to measure proximity to.</summary>
+    [Export] public Faction[] Factions = [];
+
+    /// <summary><c>true</c> if being closer to the closest unit in the group results in a high value or <c>false</c> if being further away does.</summary>
     [Export] public bool MoveCloser = true;
 
+    /// <summary>Strategy to use to measure the distance to the closest unit in the group.</summary>
     [Export] public DistanceStrategy EvaluationStrategy = DistanceStrategy.ManhattanDistance;
 
     public override double Evaluate(SimulatedAction action)
     {
         bool IsIncluded(UnitData u)
         {
-            if (SpecificUnits.Contains(u.Identity))
+            if (Units.Contains(u.Identity))
                 return true;
-            if (InFactions.Contains(u.Faction))
+            if (Factions.Contains(u.Faction))
                 return true;
-            if (AnyAlly && action.Faction.AlliedTo(u.Faction))
+            if (_allies && action.Faction.AlliedTo(u.Faction))
                 return true;
-            if (AnyEnemy && !action.Faction.AlliedTo(u.Faction))
+            if (_enemies && !action.Faction.AlliedTo(u.Faction))
                 return true;
             return false;
         }
@@ -62,5 +107,12 @@ public partial class ProximityEvaluator : ActionEvaluator
             else
                 return 1 - 1.0/costs.Min();
         }
+    }
+
+    public override void _ValidateProperty(Godot.Collections.Dictionary property)
+    {
+        base._ValidateProperty(property);
+        if ((property["name"].AsStringName() == PropertyName.Factions || property["name"].AsStringName() == PropertyName.Units) && _allies && _enemies)
+            property["usage"] = (int)PropertyUsageFlags.NoEditor;
     }
 }
