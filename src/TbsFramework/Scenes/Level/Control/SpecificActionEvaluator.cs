c3250f2c910debdb4428f4b9c34d07d9f2860b62
aroelke <alec.roelke@gmail.com>
2026-08-19 22:45:46 -0400

    Add comments to SpecificActionEvaluator

--- src/TbsFramework/Scenes/Level/Control/SpecificActionEvaluator.cs
+++ src/TbsFramework/Scenes/Level/Control/SpecificActionEvaluator.cs
@@ -3,9 +3,11 @@ using Godot;
 
 namespace TbsFramework.Scenes.Level.Control;
 
+/// <summary>Evaluates actions based on if they are part of a desired set.</summary>
 [GlobalClass, Tool]
 public partial class SpecificActionEvaluator : ActionEvaluator
 {
+    /// <summary>Set of actions. If a simulated action is in this set, it is evaluated high. Otherwise, it is evaluated low.</summary>
     [Export] public UnitAction[] TrackedActions = [];
 
     // No need to check if the unit can perform the action because it already did
