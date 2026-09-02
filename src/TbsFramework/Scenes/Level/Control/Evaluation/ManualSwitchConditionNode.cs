a70db8c6d303c9da682fc1c8e83e77c0d797a5ef
aroelke <alec.roelke@gmail.com>
2026-09-02 11:27:00 -0400

    ManualSwitchCondition -> ManualSwitchConditionNode

--- src/TbsFramework/Scenes/Level/Control/Evaluation/ManualSwitchConditionNode.cs
+++ src/TbsFramework/Scenes/Level/Control/Evaluation/ManualSwitchConditionNode.cs
@@ -4,7 +4,7 @@ namespace TbsFramework.Scenes.Level.Control.Evaluation;
 
 /// <summary>Switch condition that only switches when a function is called.</summary>
 [Tool]
-public partial class ManualSwitchCondition : SwitchConditionNode
+public partial class ManualSwitchConditionNode : SwitchConditionNode
 {
     /// <summary>Switch whether the condition is satisfied.</summary>
     public void Trigger() => Satisfied = !Satisfied;
